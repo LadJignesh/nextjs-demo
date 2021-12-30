b9c9ce2ed88c004f972a568946df03cc1af7273c
jlad <jlad@overstock.com>
2021-12-30 12:14:07 +0000

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum officia soluta, aspernatur velit, eos magnam consequatur, fuga deleniti veritatis illo facilis? Veniam facere quis quasi porro beatae distinctio ab natus!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas illo cum sapiente minima rem labore sequi, deserunt assumenda odio asperiores officia dignissimos impedit facilis saepe exercitationem dolores a consectetur reprehenderit.</p>
+
+        </div>
+    );
+}
+
+export default About;
